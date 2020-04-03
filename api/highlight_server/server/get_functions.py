fe8270e380215d19630c2044bcdc7957317cfa19
Dmitry Vilensky <pasechnyuk2004@gmail.com>
2020-04-03 12:03:12 +0300

    test

--- api/highlight_server/server/get_functions.py
+++ api/highlight_server/server/get_functions.py
@@ -112,7 +112,7 @@ def get_file_stat():
     docs = []
     for t in l_s.find({"status": {"$in": ["TRANSLATED", "NEED_CHECK", "WAITING_FOR_TRANSLATION"]}}):
         docs.append(t["status"])
-        if t["status"] in {"TRANSLATED", "NEED_CHECK"}:
+        if t["status"] in ["TRANSLATED", "NEED_CHECK"]:
             docs.append({"name": t["name"], "pieces_info": {}, "status": t["status"], "importance": l_s.find_one({"name": t["name"], "number": t["number"], "lang": t["lang"], "status": "WAITING_FOR_TRANSLATION"})["importance"]})
         # else:
         #     docs.append({"name": t["name"], "pieces_info": {"done_pieces": l_s.count_documents({"name": t["name"], "number": t["number"], "lang": t["lang"], "status": "PIECE", "translation_status": "DONE"}), "all_pieces": t["piece_number"]}, "status": t["status"], "importance": t["importance"]})
