d9d169bfb0b56a40bddb77d71ad21b6bdd38dcfd
Dmitry Vilensky <pasechnyuk2004@gmail.com>
2020-04-03 17:53:34 +0300

    big files [3]

--- php_scripts/save_file.php
+++ php_scripts/save_file.php
@@ -1,11 +1,5 @@
 <?php
 
-ini_set('max_execution_time','600');
-ini_set('max_input_time','600');
-ini_set('memory_limit','1024');
-ini_set('post_max_size','1028');	
-ini_set('upload_max_filesize','1024');
-
 $uploaddir = $_SERVER['DOCUMENT_ROOT'].'/files/';
 $uploadfile = $uploaddir . basename($_POST['name']);
 
@@ -13,6 +7,7 @@ if (move_uploaded_file($_FILES['document']['tmp_name'], $uploadfile)) {
     echo "OK";
 } else {
     echo "ERROR!";
+    echo print_r($_FILES);
 }
 
 ?>
