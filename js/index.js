28ef1dda2f015cd2074bd9172ae1d3f4782388af
Dmitry Vilensky <pasechnyuk2004@gmail.com>
2020-04-03 11:35:57 +0300

    fixed login

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function signin() {
+function try_signin_both() {
     var login_val = $("#login").val();
     var password_val = $("#password").val();
     var is_editor = $('#editor').is(':checked');
@@ -10,7 +10,8 @@ function signin() {
             method: "POST",
             data: {
                 login: login_val,
-                password: password_val
+                password: password_val,
+                type: "both"
             },
             dataType: "json"
         })
@@ -27,6 +28,42 @@ function signin() {
                 alert("Неверный логин!");
             } else if (response.code == "2002") {
                 alert("Аккаунт пока не подтверждён.");
+            }
+        })
+        .fail(function(jqXHR, status, error) {
+            console.log(error);
+        });
+}
+
+function signin() {
+    var login_val = $("#login").val();
+    var password_val = $("#password").val();
+    var is_editor = $('#editor').is(':checked');
+
+    var home_page = is_editor ? "main_editor.html" : "main.html";
+    var status = is_editor ? "chief" : "translator";
+
+    $.ajax({
+            url: "api/login",
+            method: "POST",
+            data: {
+                login: login_val,
+                password: password_val,
+                type: status
+            },
+            dataType: "json"
+        })
+        .done(function(data) {
+            response = data;
+            if (response.code == "OK") {
+                var id = response.id;
+                $.redirectGet(home_page, {
+                    user_id: id
+                });
+            } else if (response.code == "2002") {
+                alert("Аккаунт пока не подтверждён.");
+            } else {
+                try_signin_both();
             }
         })
         .fail(function(jqXHR, status, error) {
