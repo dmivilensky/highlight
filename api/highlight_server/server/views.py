2f6d6443ecebda501ffcd8cdadb927ae46092532
sevakabrits <seva.kabrits@gmail.com>
2020-04-07 18:15:03 +0300

    new edition loader

--- api/highlight_server/server/views.py
+++ api/highlight_server/server/views.py
@@ -345,6 +345,8 @@ def update_docs_cover(request):
         lgr.log("log", "loader status: ", "request = post")
         form = UploadFileForm(get_params(request), request.FILES)
         lgr.log("log", "loader status: ", form)
+        lgr.log("log", "loader status: ", request.FILES)
+        lgr.log("log", "loader status: ", form.is_valid())
         if form.is_valid():
             path = handle_uploaded_file(request.FILES['file'])
         else:
