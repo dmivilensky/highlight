75fa572530683d7591b7146fb41680106951606b
Dmitry Vilensky <pasechnyuk2004@gmail.com>
2020-04-08 21:37:40 +0300

    compose docx

--- api/highlight_server/server/filemanager.py
+++ api/highlight_server/server/filemanager.py
@@ -21,6 +21,8 @@ class MergeStatus(Enum):
 
 def convert_to(folder, source, timeout=None):
     args = [libreoffice_exec(), '--headless', '--convert-to', 'pdf', '--outdir', folder, source]
+    lgr = Logger()
+    lgr.log("log", "convertion", " ".join(args))
 
     process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=timeout)
     filename = re.search('-> (.*?) using filter', process.stdout.decode())
