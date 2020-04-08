619b5a6943627f727e259da3548b83b0ee04c879
Dmitry Vilensky <pasechnyuk2004@gmail.com>
2020-04-08 22:15:41 +0300

    compose docx

--- api/highlight_server/server/filemanager.py
+++ api/highlight_server/server/filemanager.py
@@ -25,25 +25,12 @@ def convert_to(folder, source, timeout=None):
     lgr.log("log", "convertion", " ".join(args))
 
     process = subprocess.run(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=timeout)
-    filename = re.search('-> (.*?) using filter', process.stdout.decode())
-
-    if filename is None:
-        raise LibreOfficeError(process.stdout.decode())
-    else:
-        return filename.group(1)
-
 
 def libreoffice_exec():
     # TODO: Provide support for more platforms
     if sys.platform == 'darwin':
         return '/Applications/LibreOffice.app/Contents/MacOS/soffice'
     return 'libreoffice'
-
-
-class LibreOfficeError(Exception):
-    def __init__(self, output):
-        self.output = output
-
 
 class FileManager:
     def __init__(self, root=getcwd()):
