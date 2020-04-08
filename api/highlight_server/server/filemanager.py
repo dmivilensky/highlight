94cf91e0a86c2763c35f2be4d27c0f7ddca54243
Dmitry Vilensky <pasechnyuk2004@gmail.com>
2020-04-08 23:57:32 +0300

    compose docx

--- api/highlight_server/server/filemanager.py
+++ api/highlight_server/server/filemanager.py
@@ -26,7 +26,7 @@ def convert_to(folder, source, timeout=None):
     lgr = Logger()
     lgr.log("log", "convertion", " ".join(args))
 
-    process = subprocess.run(args)
+    process = subprocess.call(args)
     for i in range(5):
         time.sleep(1)
         lgr.log("log", "convertion", str(process))
