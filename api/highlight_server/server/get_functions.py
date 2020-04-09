3cc6571183613098724945fbc6343ae76b492284
Dmitry Vilensky <pasechnyuk2004@gmail.com>
2020-04-09 19:42:34 +0300

    docs

--- api/highlight_server/server/get_functions.py
+++ api/highlight_server/server/get_functions.py
@@ -110,7 +110,7 @@ def get_users_by_doc_or_piece(rid):
         if p["translator"] not in added_ids:
             res.append(acc.find_one({"_id": ObjectId(p["translator"])}))
             added_ids.append(p["translator"])
-    return {"code": "OK", "document": added_ids}
+    return {"code": "OK", "document": res}
 
 
 def get_docs_and_trans():
