22071cd25801b5c25f7adaa1d583428843d9775e
Dmitry Vilensky <pasechnyuk2004@gmail.com>
2020-04-09 16:53:18 +0300

    docs

--- api/highlight_server/server/find_functions.py
+++ api/highlight_server/server/find_functions.py
@@ -52,7 +52,8 @@ def find_doc_by_lang(lang):
         return {"code": "3100"}
     for piece in querya:
         orig_doc = lang_storage.find_one({"name": piece["name"], "number": piece["number"], "lang": piece["lang"], "status": "WAITING_FOR_TRANSLATION"})
-        raise ValueError(str(orig_doc))
+        if piece["name"] == "q":
+            raise ValueError(str(orig_doc))
         if (piece["name"]+"#del#"+str(piece["number"])) in docs.keys():
             docs[(piece["name"]+"#del#"+str(piece["number"]))].append(piece)
         else:
