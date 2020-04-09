eb23b1b7590a3bb65d0099d674b23140669176fd
sevakabrits <seva.kabrits@gmail.com>
2020-04-09 21:44:43 +0300

    backup manager

--- api/highlight_server/server/backup_manager.py
+++ api/highlight_server/server/backup_manager.py
@@ -4,10 +4,10 @@ class Backup_Manager:
     def __init__(self):
         pass
 
-    def backup(self, where="/var/www/html/highlight.spb.ru/backups/mongo/", db="highlight", mu="", mp=""):
+    def backup(self, where="/var/www/html/highlight.spb.ru/backups/mongo/", db="highlight", mu="a", mp="a"):
         mb.backup(mu, mp, where, database=db)
 
-    def restore(self, where="/var/www/html/highlight.spb.ru/backups/mongo/", what="backup" + ".tbz", mu="", mp=""):
+    def restore(self, where="/var/www/html/highlight.spb.ru/backups/mongo/", what="backup" + ".tbz", mu="a", mp="a"):
         mb.restore(mu, mp, where+what)
 
 
